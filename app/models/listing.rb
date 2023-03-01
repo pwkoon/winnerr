99d73544f627c21eeec9f92dffe291633bb16aa3
wankoon puah puahwankoon@gmail.com <puahwankoon@gmail.com>
2023-03-01 18:40:14 +0800

    done claudinary

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -5,6 +5,7 @@ class Listing < ApplicationRecord
   has_many :prices, dependent: :destroy
   has_many :projects, dependent: :destroy
   validates :title, :description, presence: true
+  has_one_attached :photo
   # TITLE = %w[Web-Design Graphic&Design Writing&Translation Music&Audio Video&Animation Programming&Tech]
   # validates :category_id, inclusion: { in: TITLE }
 end
